6464e9773aa57bdc15d0661f0a2be5986a6cb601
scottschmidl <sjschmidl@gmail.com>
2022-06-07 23:11:38 -0500

    add files for cronjob

---
+++ test.py
@@ -0,0 +1,23 @@
+import logging
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(dir_path, "test_log.log")
+
+# could do just, but don't
+# filename = "test_log.log"
+
+# logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler(filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(file_handler)
+
+def main():
+    logging.info("test")
+
+if __name__ == "__main__":
+    main()
