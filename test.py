ddc37dc3cf5eee1f526fefb7df25c5bfbbbcb9d2
scottschmidl <sjschmidl@gmail.com>
2022-06-09 21:50:56 -0500

    cron job not working but py files are

--- test.py
+++ test.py
@@ -2,29 +2,27 @@ import logging
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = os.path.join(dir_path, "test_log.log")
-
 ## could do just, but don't
 # filename = "test_log.log"
+filename = os.path.join(dir_path, "test_log.log")
 
-# logger
-formatter = "%(asctime)s - %(levelname)s - %(message)s"
-logging.basicConfig(filename=filename, format=formatter, encoding='utf-8', level=logging.INFO)
-
-## the below is not working. read: https://docs.python.org/3/howto/logging.html to understand the below better
 ## Logger
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
+# formatter = "%(asctime)s - %(levelname)s - %(message)s"
+# logging.basicConfig(filename=filename, format=formatter, encoding='utf-8', level=logging.INFO)
 
-# file handler
-# file_handler = logging.FileHandler(filename)
-# file_handler.setLevel(logging.INFO)
-# file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+# the below is not working. read: https://docs.python.org/3/howto/logging.html to understand the below better
+# Logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-# logger.addHandler(file_handler)
+# File Handler
+file_handler = logging.FileHandler(filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(file_handler)
 
 def do_logging():
-    logging.info("test")
+    logger.info("test")
 
 if __name__ == "__main__":
     do_logging()
